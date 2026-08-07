d03eceaad656ae3b4656a30d29c742d81a9caeed
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-08-07 11:00:07 +0900

    first commit

---
+++ src/main/java/in/techcamp/furima_c/FurimaCApplication.java
@@ -0,0 +1,13 @@
+package in.techcamp.furima_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurimaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurimaCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/furima_c/FurimaCApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.furima_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurimaCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
