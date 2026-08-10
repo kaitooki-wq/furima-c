687cde3979624cf469a3c4b875b7e730c1671201
ldw-0807 <d.lee@infomart.co.jp>
2026-08-10 16:32:12 +0900

    implement edit-page
    - edit.htmlの変更
    - テストコードの実装
    - 依存関係の整え
    （ユーザー管理、商品編集のテスト完了）

--- src/test/java/in/techcamp/furima_c/controller/ItemControllerTest.java
+++ src/test/java/in/techcamp/furima_c/controller/ItemControllerTest.java
@@ -1,66 +1,79 @@
 package in.techcamp.furima_c.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
 import in.techcamp.furima_c.dto.ItemEditForm;
 import in.techcamp.furima_c.service.ItemService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@WebMvcTest(ItemController.class)
+//　テスト時、Flywayの自動実行を無効化するために、@SpringBootTestにproperties属性を追加
+@SpringBootTest(properties = "spring.flyway.enabled=false")
+@AutoConfigureMockMvc(addFilters = false) // Spring Securityのフィルターを無効化
 class ItemControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @MockitoBean
-    private ItemService itemService;
+  @MockBean
+  private ItemService itemService;
 
-    @Test
-    @DisplayName("編集画面の表示が成功すること")
-    void editFormTest() throws Exception {
-        // given
-        ItemEditForm form = ItemEditForm.builder().id(1L).name("テスト").build();
-        given(itemService.getItemForEdit(anyLong())).willReturn(form);
+  @Test
+  @DisplayName("GET /items/{itemId}/edit - 編集画面が正常に表示される")
+  void showEditForm_Success() throws Exception {
+    ItemEditForm form = ItemEditForm.builder()
+        .id(1L)
+        .name("テスト商品名")
+        .description("テスト商品説明")
+        .price(1000)
+        .build();
 
-        // when & then
-        mockMvc.perform(get("/items/1/edit"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("items/edit"))
-                .andExpect(model().attributeExists("itemForm"));
-    }
+    when(itemService.getItemForEdit(anyLong())).thenReturn(form);
 
-    @Test
-    @DisplayName("タイトルが空の場合、編集画面に戻ること")
-    void updateItem_validationFail() throws Exception {
-        // when & then (タイトルが空の場合)
-        mockMvc.perform(post("/items/1/edit")
-                        .param("name", "")
-                        .param("description", "설명")
-                        .param("price", "10000"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("items/edit"))
-                .andExpect(model().hasErrors());
-    }
+    mockMvc.perform(MockMvcRequestBuilders.get("/items/1/edit"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.view().name("items/edit"))
+        .andExpect(MockMvcResultMatchers.model().attributeExists("itemForm"));
+  }
 
-    @Test
-    @DisplayName("編集処理が成功すること")
-    void updateItem_success() throws Exception {
-        // when & then
-        mockMvc.perform(post("/items/1/edit")
-                        .param("name", "new name")
-                        .param("description", "new description")
-                        .param("categoryId", "1")
-                        .param("price", "15000"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/items/1"));
-    }
+  @Test
+  @DisplayName("POST /items/{itemId}/edit - 未入力項目がある場合、バリデーションエラーになり編集画面に戻る")
+  void updateItem_ValidationError_ReturnsEditView() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/items/1/edit")
+            .param("name", "")
+            .param("description", "")
+            .param("price", "100"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.view().name("items/edit"))
+        .andExpect(MockMvcResultMatchers.model().hasErrors());
+  }
+
+  @Test
+  @DisplayName("POST /items/{itemId}/edit - 正常的にデータ送信時、詳細画面へリダイレクトされる")
+  void updateItem_Success_RedirectsToDetail() throws Exception {
+    doNothing().when(itemService).updateItem(anyLong(), any());
+
+    mockMvc.perform(MockMvcRequestBuilders.post("/items/1/edit")
+            .param("name", "修正された商品名")
+            .param("description", "修正された商品説明")
+            .param("categoryId", "1")
+            .param("price", "15000")
+            .param("condition", "1")
+            .param("shippingPayer", "1")
+            .param("prefectureId", "13")
+            .param("shippingDays", "1")
+            .param("status", "0"))
+        .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+        .andExpect(MockMvcResultMatchers.redirectedUrl("/items/1"));
+  }
 }
