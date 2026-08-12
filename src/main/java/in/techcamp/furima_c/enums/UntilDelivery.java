968324f9394fe8b8f9042d36a9fe9b7dd9c9ffed
akira <a.ono@infomart.co.jp>
2026-08-12 08:33:02 +0000

    Enum作成

---
+++ src/main/java/in/techcamp/furima_c/enums/UntilDelivery.java
@@ -0,0 +1,33 @@
+package in.techcamp.furima_c.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UntilDelivery {
+    FAST(1, "1〜2日で発送"),
+    NORMAL(2, "2〜3日で発送"),
+    SLOW(3, "4〜7日で発送");
+
+    private final int code;
+    private final String displayName;
+
+    public static UntilDelivery fromCode(int code) {
+        for (UntilDelivery until : UntilDelivery.values()) {
+            if (until.getCode() == code) {
+                return until;
+            }
+        }
+        throw new IllegalArgumentException("不正な発送日数コードです: " + code);
+    }
+
+    public static UntilDelivery fromDisplayName(String displayName) {
+        for (UntilDelivery until : UntilDelivery.values()) {
+            if (until.getDisplayName().equals(displayName)) {
+                return until;
+            }
+        }
+        throw new IllegalArgumentException("不正な発送日数表示です: " + displayName);
+    }
+}
