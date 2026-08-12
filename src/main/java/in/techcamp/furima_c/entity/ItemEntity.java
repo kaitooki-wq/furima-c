105246ab82caa92b42a587e02902631fab640c43
ksekita <k.sekita@infomart.co.jp>
2026-08-12 11:21:57 +0900

    add-ItemEntity

---
+++ src/main/java/in/techcamp/furima_c/entity/ItemEntity.java
@@ -0,0 +1,19 @@
+package in.techcamp.furima_c.entity;
+
+import lombok.Data;
+
+@Data
+public class ItemEntity {
+    private Long id;
+    private Long sellerId;
+    private Long buyerId;
+    private String name;
+    private String description;
+    private Long categoryId;
+    private Integer condition;
+    private Integer shippingPayer;
+    private Integer prefectureId;
+    private Integer shippingDays;
+    private Integer price;
+    private Integer status;
+}
