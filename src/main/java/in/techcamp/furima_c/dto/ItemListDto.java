c8185c1829e2f53a6b4aed58fe325310fa345ac9
ksekita <k.sekita@infomart.co.jp>
2026-08-13 11:48:14 +0900

    fix multipartFile

--- src/main/java/in/techcamp/furima_c/dto/ItemListDto.java
+++ src/main/java/in/techcamp/furima_c/dto/ItemListDto.java
@@ -1,12 +1,14 @@
 package in.techcamp.furima_c.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
 public class ItemListDto {
     private Long id;
     private String name;
-    private String image;
+    private MultipartFile image;
     private Integer price;
     private Integer shippingPayer;
 }
