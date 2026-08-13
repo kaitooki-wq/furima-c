286a3463035df94724ccff70500b064ded801049
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-08-13 04:18:41 +0000

    商品出品　DTO

---
+++ src/main/java/in/techcamp/furima_c/dto/ItemCreateDto.java
@@ -0,0 +1,40 @@
+package in.techcamp.furima_c.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ItemCreateDto {
+    @NotBlank(message="商品名は必須です")
+   private String name;
+   @NotBlank(message = "商品の説明は必須です")
+    private String description;
+    @NotNull(message = "選択してください")
+    @Min(1)
+    private Long categoryId;
+     @NotNull(message = "選択してください")
+    @Min(1)
+    private Integer condition;
+     @NotNull(message = "選択してください")
+     @Min(1)
+    private Integer shippingPayer;
+     @NotNull(message = "選択してください")
+    @Min(1)
+    private Integer prefectureId;
+     @NotNull(message = "選択してください")
+     @Min(1)
+    private Integer shippingDays;
+    @Min(300)@Max(9999999)
+    @NotNull(message = "価格を入力してください")
+    private Integer price;
+    @NotNull(message = "商品画像を添付してください")
+    private MultipartFile image;
+
+
+    
+}
